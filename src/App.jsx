795385c69d085d1f431657d29ac8cd24e075e80d
hee123hee <gmldnjsl5@naver.com>
2025-02-10 18:35:19 +0900

    1-4-5. props children

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
-function App() {
+import ExpenseList from "./components/expenses/ExpenseList.jsx";
+
+
+/*
+    jsx: 리액트에서 사용하는 특수한 js문법, 태그를 그대로 쓰면 알아서 변환
+
+    - 규칙:
+    1. return 안에 있는 태그는 반드시 하나의 태그로 묶여야 함.
+    2. 빈 태그(닫는 태그가 없는)는 반드시 />로 마감.
+    3. 태그의 class속성은 자바스크립트 키워드 class와 겹쳐서 className으로 표기.
+    4. 의미없는 부모는 <React.Fragment>로 감싸면 됨.
+    5. 변수값이나 함수를 출력할 때는 {}로 감사면 됨.
+ */
+
+// const $h1 = React.createElement('h1', null, '안녕 리액트 !!');
+
+const App = () => {
+
+    // 서버에서 지출항목 JSON이 응답됨
+    const expenses = [
+        {
+            title: '치킨',
+            price: 30000,
+            date: new Date(2025, 2 - 1, 3),
+        },
+        {
+            title: '떡볶이',
+            price: 4000,
+            date: new Date(2025, 9 - 1, 30),
+        },
+        {
+            title: '헬스장등록',
+            price: 22000,
+            date: new Date(2025, 6 - 1, 8),
+        },
+    ];
 
 
     return (
+        <>
+
+            <ExpenseList expenses={expenses}/>
+        </>
+    );
 }
 
-export default App
+export default App;
+
+
---
+++ src/components/expenses/ExpenseItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
+import Card from '../ui/Card';
+
+const ExpenseItem = ({expense}) => {
+    // console.log('props:' ,props);
+    const {title, price, date} = expense;
+
+    // 원화 표기법으로 변환
+    const formatPrice = new Intl.NumberFormat('ko-KR').format(price);
+
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate expenseDate={date}/>
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>{formatPrice}원</div>
+            </div>
+        </Card>
+    );
+};
+
+
+export default ExpenseItem;
