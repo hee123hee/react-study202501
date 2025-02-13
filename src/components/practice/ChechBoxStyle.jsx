1589202732f43d3cb66b672e47fdc028d5e27513
hee123hee <gmldnjsl5@naver.com>
2025-02-13 17:17:04 +0900

    2-3-4. 연습문제 해설

--- src/components/practice/ChechBoxStyle.jsx
+++ src/components/practice/ChechBoxStyle.jsx
@@ -1,20 +1,29 @@
-
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './CheckBoxStyle.css';
+
 function CheckBoxStyle() {
     /*
       1. input[checkbox]에 change이벤트가 걸려서
       2. check 상태가 바뀔 때마다 상태변수를 논리값으로 업데이트하여
       3. label의 클래스를 유동적으로 변경해야함.
     */
+
+    const [checked, setChecked] = useState(false);
+    const handleCheckbox = e => {
+        // console.log('chechbox click!!!!');
+        setChecked(!checked);
+    };
+
     return (
         <div className='checkbox-container'>
             <input
                 type='checkbox'
                 id='styled-checkbox'
+                onChange={handleCheckbox}
             />
-            <label className='unchecked' htmlFor='styled-checkbox'>Check me!</label>
+            <label className={checked ? 'checked' : 'unChecked'} htmlFor='styled-checkbox'>Check me!</label>
         </div>
     );
 }
+
 export default CheckBoxStyle;
