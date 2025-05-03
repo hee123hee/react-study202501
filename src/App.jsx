f810d129013d8f523c44b0e9991d7b1f25a2d5a3
123hee123 <gmldnjsl5@naver.com>
2025-05-04 05:25:40 +0900

    지출항목 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import Hello from './components/Hello'
-import Bye from "./components/Bye.jsx";
+import ExpenseItem from "./components/expenses/ExpenseItem";
 
 /*
       jsx: 리액트에서 사용하는 특수한 js문법, 태그를 그대로 쓰면 알아서 변환
@@ -12,16 +11,14 @@ import Bye from "./components/Bye.jsx";
       5. 변수 값이나 함수를 출력할 때는 {}로 감싸면 됨
  */
 
-function App() {
+const App = () => {
     // const $h1 = React.createElement('h1', null, '안녕 리액트');
 
     // 렌더링 수행
     // return $h1;
     return (
         <>
-            <Bye />
-            <Hello/>
-            <Bye />
+            <ExpenseItem/>
         </>
     )
 }
