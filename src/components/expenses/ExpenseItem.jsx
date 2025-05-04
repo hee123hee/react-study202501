84ea068848d390eeb116957666b5655cfb95fda2
123hee123 <gmldnjsl5@naver.com>
2025-05-04 22:21:44 +0900

    # 2-1-1. 이벤트 바인딩

--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -1,6 +1,7 @@
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate.jsx";
 import Card from "../ui/Card.jsx";
+import log from "eslint-plugin-react/lib/util/log.js";
 
 const ExpenseItem = ({expense}) => {
     // console.log('props', props);
@@ -9,13 +10,25 @@ const ExpenseItem = ({expense}) => {
     // 원화 표기법으로 변환
     const formatPrice = new Intl.NumberFormat('ko-KR').format(price)
 
+    // 이벤트 핸들러 정의
+    function handleClick(e) {
+        console.log('click!!');
+        console.log(e.target)
+    }
+
     return (
-        <Card className='expense-item'>
-            <ExpenseDate expenseDate={date} />
+        <Card className='expense-item' onClick={(e)=>console.log('이벤투!')}>
+            <ExpenseDate expenseDate={date}/>
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>{formatPrice}원</div>
             </div>
+            <button id="btn1" onClick={handleClick}>버튼1</button>
+            <button id="btn2"
+                    onMouseLeave={(e) => alert('나가기~!')}
+                    onMouseOver={(e) => alert('이벤트!!')}>
+                버튼2
+            </button>
         </Card>
     );
 };
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Card.css'
 
-const Card = ({children, className}) => {
+const Card = ({children, className, onClick}) => {
 
 
     const cn = `card ${className}`
     return (
-        <div className={cn}>
+        <div className={cn} onClick={onClick}>
             {/* 유동적인 구조인 경우 */}
             {children}
         </div>
